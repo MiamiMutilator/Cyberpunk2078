0c3b6ed0a865b46b6c461f609f78906b98b5b19f
Anthony Rattigan <tonyratt24@gmail.com>
2025-03-30 22:55:21 -0400

    Add Controller Dash Support
    Default Circle/B Dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,7 +31,8 @@ public class PlayerController : MonoBehaviour
     bool vulverable;
     [SerializeField] float blinkDistance = 5f;
     [SerializeField] float blinkCooldown = 1f;
-    public KeyCode blinkKeybind = KeyCode.C;
+    public KeyCode blinkKeyboard = KeyCode.C;
+    public KeyCode blinkController = KeyCode.Joystick1Button2;
     float blinkTimer;
     bool canBlink;
     float horizontal, vertical;
@@ -136,7 +137,7 @@ public class PlayerController : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyDown(blinkKeybind) && canBlink)
+        if ((Input.GetKeyDown(blinkKeyboard) || Input.GetKeyDown(blinkController)) && canBlink)
             StartCoroutine(Blink());
         else
         {
