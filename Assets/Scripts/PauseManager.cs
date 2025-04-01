ede4c329684af547d54dd8a04a35b871444515e3
Anthony Rattigan <tonyratt24@gmail.com>
2025-03-31 21:39:22 -0400

    Pause Menu Bugfix

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PauseManager : MonoBehaviour
 {
     private bool isPaused = false;
-    bool enemyFound = false;
     public GameObject pauseMenuUI;
     public GameObject howtoplayUI;
     public GameObject creditsUI;
@@ -26,50 +25,38 @@ public class PauseManager : MonoBehaviour
         {
             if (isPaused)
             {
-                UnityEngine.Cursor.lockState = CursorLockMode.Locked;
                 ResumeGame(); 
             }
             else
             {
-                Cursor.lockState = CursorLockMode.None;
                 PauseGame();
             }
-        }
-        if (pauseMenuUI.activeInHierarchy == false)
-        {
-            ResumeGame();
         }
     }
 
     public void PauseGame()
     {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         Time.timeScale = 0f;
         isPaused = true;
         pauseMenuUI.SetActive(true);
         player.SetActive(false);
-        if (enemyFound) enemyAudio.Pause();
         //PlayerMovement.instance.SetPlayerStatus(false);
         reticle.SetActive(false);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        
     }
 
     public void ResumeGame()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1f;
         isPaused = false;
         pauseMenuUI.SetActive(false);
         player.SetActive(true);
-        if (enemyFound) enemyAudio.Play();
         //PlayerMovement.instance.SetPlayerStatus(true);
         reticle.SetActive(true);
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    public void FindEnemy()
-    {
-        enemyAudio = GameObject.Find("Enemy").GetComponent<AudioSource>();
-        enemyFound = true;
+        
     }
 }
