91fd088c96811fb811db9402d6f9c6a830b605db
LucasSossai <lucas@sossai.com>
2025-04-12 19:53:27 -0400

    fixed missing semi colon on game manager script

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -226,6 +226,6 @@ public class GameManager : MonoBehaviour
 
     public bool AllLevelsComplete()
     {
-        return (sewerComplete && alleyComplete && roofComplete)
+        return (sewerComplete && alleyComplete && roofComplete);
     }
 }
