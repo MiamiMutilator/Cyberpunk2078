d81f40fe959400bb30b1e3f54e779c52a6ea3b55
Anthony Rattigan <tonyratt24@gmail.com>
2025-04-13 20:20:01 -0400

    win screen cursor fix

--- Assets/Scripts/WinLoseScene.cs
+++ Assets/Scripts/WinLoseScene.cs
@@ -6,6 +6,7 @@ public class WinLoseScene : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
     public void ReturnToMainMenu()
     {
